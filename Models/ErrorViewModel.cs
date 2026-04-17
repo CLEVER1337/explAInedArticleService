1a20a4b06d73c615cf1632c10847ca4666fa16c8
clever <sanya.veselkov.811@gmail.com>
2026-04-17 20:28:56 +0300

    init

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace explAInedArticleService.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
